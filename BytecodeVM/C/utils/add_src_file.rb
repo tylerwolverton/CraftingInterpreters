9a7f95f6aa0c75da61144cf0c33905917894084e
Tyler Wolverton <tylerwolvert@gmail.com>
2025-01-03 16:04:01 +0000

    Updated Makefile and added add file script
    - Makefile can handle header updates
    - add_src_file script to create c file and add to Makefile
    Note: Headers don't need to be specified in Makefile, so no need to write a script to add those

---
+++ BytecodeVM/C/utils/add_src_file.rb
@@ -0,0 +1,25 @@
+require 'fileutils'
+
+if ARGV[0] == nil
+    puts "No filename specified. usage: add_files newFile"
+    return
+end
+
+new_src_name = ARGV[0] + ".c"
+new_obj_name = ARGV[0] + ".o"
+
+#add new file
+FileUtils.touch "src/" + new_src_name
+
+#update the makefile
+array_of_lines = File.open("Makefile", "r").readlines.map( &:chomp )
+
+array_of_lines.each { |line|
+    if line[0..6] == "OBJECTS"
+        line << " " << new_obj_name
+    end
+} 
+
+File.open( "Makefile", "w" ) do |f|
+    f.puts array_of_lines
+end
